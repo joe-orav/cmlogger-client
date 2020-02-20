3523c4495cbfdb85f438304a2c7e25eb1c00bb40
Joseph Oravbiere <joravbiere@gmail.com>
2020-02-19 23:31:26 -0500

    Create new react app and add layout

---
+++ src/app.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Layouts from "./layouts";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+
+function App() {
+  return (
+    <Router forceRefresh={true}>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/overview" />
+        </Route>
+        <Route path="/overview">
+          <Layouts.NavLayout user={{}}><Overview /></Layouts.NavLayout>
+        </Route>
+        <Route path="/cars">
+          <Layouts.NavLayout user={{}}><Cars /></Layouts.NavLayout>
+        </Route>
+        <Route path="/service-history">
+          <Layouts.NavLayout user={{}}><ServiceHistory /></Layouts.NavLayout>
+        </Route>
+        <Route path="/settings">
+          <Layouts.NavLayout user={{}}><Settings /></Layouts.NavLayout>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+function Overview() {
+  return <p>Overview</p>
+}
+
+function Cars() {
+  return <p>Cars</p>
+}
+
+function ServiceHistory() {
+  return <p>Service History</p>
+}
+
+function Settings() {
+  return <p>Settings</p>
+}
+
+
+export default App;
